942409fb509b6d0135db97dbc99eb736015bd0e6
PinkPriest <116888083+PinkPriest@users.noreply.github.com>
2024-05-01 23:36:05 +0300

    Update models.py

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -4,7 +4,7 @@ class Phone(models.Model):
     id = models.CharField(primary_key=True)
     name = models.CharField()
     price = models.IntegerField()
-    image = models.IntegerField()
+    image = models.URLField()
     release_date = models.DateTimeField()
     lte_exists = models.BooleanField()
     slug = models.SlugField(unique=True, verbose_name=name)
