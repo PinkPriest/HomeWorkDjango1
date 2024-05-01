fa31f6dbefb5de22c7b30275130193cae3a7a870
PinkPriest <116888083+PinkPriest@users.noreply.github.com>
2024-05-01 23:37:47 +0300

    Update import_phones.py

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -14,5 +14,6 @@ class Command(BaseCommand):
         for phone in phones:
             phone = Phone(id=phone.get("id"), name=phone.get("name"), image=phone.get("image"),
                           price=phone.get("price"), release_date=phone.get("release_date"),
-                          lte_exists=phone.get("lte_exists"))
+                          lte_exists=phone.get("lte_exists"), slug=phone.get("slug"))
             phone.save()
+
